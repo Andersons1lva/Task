9f54056a311fd9c04527a69b8bb65fcf6debb58f
andersons1lva <andersons1lva@yahoo.com.br>
2022-09-05 20:06:34 -0300

    Creating registerUser and validaData functions inside the RegisterFragment

--- app/src/main/java/com/anderson/task/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/anderson/task/ui/auth/RegisterFragment.kt
@@ -1,22 +1,59 @@
 package com.anderson.task.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.anderson.task.R
+import android.widget.Toast
+import androidx.core.view.isVisible
+import androidx.fragment.app.Fragment
+import com.anderson.task.databinding.FragmentRegisterBinding
 
 
 class RegisterFragment : Fragment() {
 
+    private var _binding: FragmentRegisterBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_register, container, false)
+    ): View {
+        // Inflate the layout for this fragment / implementação do viewbinding
+        _binding = FragmentRegisterBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
+    //Função valida dados
+    private fun validaData(){
+        val email = binding.edtEmail.text.toString().trim()
+        val password = binding.edtPassword.text.toString().trim()
+        // verificação se foi digitado algo no campo
+        if (email.isNotEmpty()){
+            if (password.isNotEmpty()){
+
+                // após verificação exibir progressBar
+                binding.progressBar.isVisible = true
+
+                // após o progressBar chamar função registerUser
+                registerUser(email,password)
+
+            }else{
+                Toast.makeText(requireContext(),"Informe sua senha",Toast.LENGTH_SHORT).show()
+            }
+        }else{
+            Toast.makeText(requireContext(),"Informe seu email",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    //função responsavel de salvar os dados no Firebase
+    private fun registerUser(email: String, password: String){
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
 
 }
