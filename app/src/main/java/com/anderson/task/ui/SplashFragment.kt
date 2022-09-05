7c8193f42074fe99207fda5eaff9e3ca6d2d58e2
andersons1lva <andersons1lva@yahoo.com.br>
2022-09-05 18:48:59 -0300

    Creation of checkAuth function in SplashFragment and initClicks function in LoginFragment.

--- app/src/main/java/com/anderson/task/ui/SplashFragment.kt
+++ app/src/main/java/com/anderson/task/ui/SplashFragment.kt
@@ -1,10 +1,14 @@
 package com.anderson.task.ui
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.anderson.task.R
 import com.anderson.task.databinding.FragmentSplashBinding
 
 class SplashFragment : Fragment() {
@@ -16,18 +20,25 @@ class SplashFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        //Configuração do viewbinding
+        //Configuração do viewbinding / Inflate the layout for this fragment
         _binding = FragmentSplashBinding.inflate(inflater,container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Implementação do tempo da Splash
+        Handler(Looper.getMainLooper()).postDelayed(this::checkAuth,3000)
+    }
+
+    //Método que check se usuário está logado
+    private fun checkAuth(){
+        //navegação da Splash para tela de login(action_splashFragment_to_loginFragment) e o id da ação criada entre a splash e o login
+        findNavController().navigate(R.id.action_splashFragment_to_loginFragment)
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
-    }
-
-    //Método que check se usuário está logado
-    private fun checkAuth(){
-
     }
 }
--- app/src/main/java/com/anderson/task/ui/auth/LoginFragment.kt
+++ app/src/main/java/com/anderson/task/ui/auth/LoginFragment.kt
@@ -1,20 +1,48 @@
 package com.anderson.task.ui.auth
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.anderson.task.R
+import com.anderson.task.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false)
+    ): View {
+        //Configuração do viewbinding / Inflate the layout for this fragment
+        _binding = FragmentLoginBinding.inflate(inflater,container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initClicks()
+    }
+
+    //Função para ouvir os enventos de clicks
+    private fun initClicks(){
+        binding.btnRegister.setOnClickListener {
+            //captura o envento de click, fazendo a navegação entre os fragment (Login com register)
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
+        binding.btnRecover.setOnClickListener {
+            //captura o envento de click, fazendo a navegação entre os fragment (Login com recover)
+            findNavController().navigate(R.id.action_loginFragment_to_recoverAccountFragment)
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
  }
