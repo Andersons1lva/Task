7eeb90568d8cd15e3859333e9137f85013d34475
andersons1lva <andersons1lva@yahoo.com.br>
2022-09-05 21:14:00 -0300

    FragmentHome creation and Firebase configuration inside FragmentRegister

--- app/src/main/java/com/anderson/task/ui/auth/RegisterFragment.kt
+++ app/src/main/java/com/anderson/task/ui/auth/RegisterFragment.kt
@@ -7,13 +7,20 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.anderson.task.R
 import com.anderson.task.databinding.FragmentRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 
 class RegisterFragment : Fragment() {
 
     private var _binding: FragmentRegisterBinding? = null
     private val binding get() = _binding!!
+
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,31 +31,55 @@ class RegisterFragment : Fragment() {
         return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+        //inicialização do metodo
+        initClicks()
+    }
+
+    //para pegar quando clicar no criar conta
+    private fun initClicks(){
+        binding.btnRegister.setOnClickListener { validaData() }
+    }
+
     //Função valida dados
-    private fun validaData(){
+    private fun validaData() {
         val email = binding.edtEmail.text.toString().trim()
         val password = binding.edtPassword.text.toString().trim()
         // verificação se foi digitado algo no campo
-        if (email.isNotEmpty()){
-            if (password.isNotEmpty()){
+        if (email.isNotEmpty()) {
+            if (password.isNotEmpty()) {
 
                 // após verificação exibir progressBar
                 binding.progressBar.isVisible = true
 
                 // após o progressBar chamar função registerUser
-                registerUser(email,password)
+                registerUser(email, password)
 
-            }else{
-                Toast.makeText(requireContext(),"Informe sua senha",Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(requireContext(), "Informe sua senha", Toast.LENGTH_SHORT).show()
             }
-        }else{
-            Toast.makeText(requireContext(),"Informe seu email",Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(requireContext(), "Informe seu email", Toast.LENGTH_SHORT).show()
         }
     }
 
     //função responsavel de salvar os dados no Firebase
-    private fun registerUser(email: String, password: String){
-
+    private fun registerUser(email: String, password: String) {
+        //Criação de email e senha
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    //se criação ok direcionar para homeFragment
+                    findNavController().navigate(R.id.action_registerFragment_to_homeFragment)
+                } else {
+                    //se não mostre a progressbar
+                    binding.progressBar.isVisible = false
+                }
+            }
     }
 
     override fun onDestroy() {
